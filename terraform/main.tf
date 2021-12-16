9ed754fcccd38fc0e03d155df24050b115e0dc3f
Brian Schreier <bschreier@bandwidth.com>
2021-12-16 13:30:27 -0500

    Add demo 2 account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "request-demo" {
 
   account_customizations_name = "sandbox"
 }
+
+module "request-demo2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "bschreier+request-demo2@bandwidth.com"
+    AccountName               = "request-demo2"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "bschreier+request-demo2@bandwidth.com"
+    SSOUserFirstName          = "request"
+    SSOUserLastName           = "demo2"
+  }
+
+  account_tags = {
+    "Demo" = "AFT-account-vending"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Brian Schreier"
+    change_reason       = "Demo number 2 account vending via aft"
+  }
+
+  custom_fields = {
+    group = "demo"
+  }
+
+  account_customizations_name = "sandbox"
+}
